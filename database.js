8209d96207cc0841d0de353a18d06ef6fb53ebef
isaacwsolomon <isaacwsolomon@gmail.com>
2025-02-11 15:21:23 +0000

    Created database.js and 3 tables

---
+++ database.js
@@ -0,0 +1,65 @@
+const sqlite3 = require('sqlite3').verbose();
+let sql1, sql2, sql3;
+
+//connect to DB
+const db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE, (err)=>{
+    if (err) return console.error(err.message);
+})
+//Create Table
+  sql1 = `CREATE TABLE IF NOT EXISTS gamedetails(id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  code TEXT UNIQUE, 
+  leader TEXT, 
+  max_cards INTEGER)`
+  db.run(sql1);
+
+sql2 = `CREATE TABLE IF NOT EXISTS players(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_code TEXT,
+    name TEXT,
+    team INTEGER,
+    FOREIGN KEY(game_code) REFERENCES gamedetails(code))`
+
+db.run(sql2);
+
+sql3 = (`CREATE TABLE IF NOT EXISTS cards (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_code TEXT,
+    player_id INTEGER,
+    content TEXT,
+    FOREIGN KEY(game_code) REFERENCES gamedetails(code),
+    FOREIGN KEY(player_id) REFERENCES players(id)
+)`);
+db.run(sql3);
+
+//Drop Table
+//db.run('DROP TABLE gamedetails');
+
+
+// Insert data into table
+// sql = `INSERT INTO gamedetails(code, leader, max_cards)VALUES(?,?,?)`;
+// db.run(sql,[345,"Josh", 6], (err)=>{
+//     if (err) return console.error(err.message);h
+// });
+
+// Update data
+// sql =  `UPDATE gamedetails SET leader = ? where id = ?`;
+// db.run(sql,["Zoe", 1], (err)=>{
+//     if (err) return console.error(err.message);
+// });
+
+// // Delete data
+//  sql =  `DELETE FROM gamedetails where id = ?`;
+// db.run(sql,[1], (err)=>{
+//     if (err) return console.error(err.message);
+// });
+
+// Query database
+// sql = `SELECT * FROM gamedetails`;
+// db.all(sql, [], (err, rows)=>{
+//     if (err) return console.error(err.message);
+//     rows.forEach(row=>{
+//         console.log(row);
+//     })
+// })
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 var express = require('express') 
 var app = express(); 
 
-const sqlite3 = require('sqlite3').verbose();
-let sql;
+const db = require('./database'); // Import the database
 
-//connect to DB
-const db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE, (err)=>{
-    if (err) return console.error(err.message);
-})
-//Create Table
-
-//Drop Table
 // Route for the home page
 app.get("/", (req, res) => {
     res.sendFile(__dirname +"/" + "views/homepage.html"); // Render the homepage view
