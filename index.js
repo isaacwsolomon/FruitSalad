8fe4392b7f92f34534ac457bc5c4c29f37453a72
isaacwsolomon <isaacwsolomon@gmail.com>
2025-02-11 13:44:53 +0000

    Created html pages and added routes to them in index file

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express') 
+var app = express(); 
+
+// Route for the home page
+app.get("/", (req, res) => {
+    res.sendFile(__dirname +"/" + "views/homepage.html"); // Render the homepage view
+});
+
+app.get("/joingame", (req, res) => {
+    res.sendFile(__dirname + "/" + "views/joingame.html"); // Render joingame page
+})
+
+app.get("/creategame", (req,res) => {
+    res.sendFile(__dirname + "/" + "views/creategame.html");
+})
+app.listen(3004, () => { 
+    console.log("Server is listening") 
+})
